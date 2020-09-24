4adecd12ac2b220e8372dcf4e005b97532de7abf
Andie Grant <aegrant08@gmail.com>
2020-09-23 22:08:42 -0400

    added input variables

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-// Assignment Code
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var number = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialCharacters = ["!", "@", "#", "$", "%", "^", "*", "(", ")", "-", "_", "=", "+", "[", "]", "{", "}", ",", ".", "?", "/", ">", "<", "|"];
+
+var confirmLowercase = confirm("Will this password include lowercase letters?")
+var confirmUppercase = confirm("Will this password include uppercase letters?")
+var confirmNumber = confirm("Will this password include numbers?")
+var confirmSpecialCharacters = confirm("Will this password include special characters?")
+// Assignment Code - html reference goes  at bottom of code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
