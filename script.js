18704098d4ada3d537766fe0052a42472dd2cc61
Andie Grant <aegrant08@gmail.com>
2020-09-30 18:02:57 -0400

    prompts and confirm

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
-var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var number = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var specialCharacters = ["!", "@", "#", "$", "%", "^", "*", "(", ")", "-", "_", "=", "+", "[", "]", "{", "}", ",", ".", "?", "/", ">", "<", "|"];
+// Arrays to generate password
+let lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+let uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+let number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+let symbol = ["!", "@", "#", "$", "%", "^", "*", "(", ")", "-", "_", "=", "+", "[", "]", "{", "}", ",", ".", "?", "/", ">", "<", "|"];
 
-var confirmLowercase = confirm("Will this password include lowercase letters?")
-var confirmUppercase = confirm("Will this password include uppercase letters?")
-var confirmNumber = confirm("Will this password include numbers?")
-var confirmSpecialCharacters = confirm("Will this password include special characters?")
-// Assignment Code - html reference goes  at bottom of code
+var length = Number(prompt("How many characters would you like your password to be?"));
+while (isNaN(length)) || length < 8 || length > 128) length = Number(prompt("Length must be 8 - 128 characters!"));
+
+let getRndLower = lowercase[Math.floor(Math.random() * lowercase.length)];
+
+let getRndUpper = uppercase[Math.floor(Math.random() * uppercase.length)];
+
+let getRndNumber = number[Math.floor(Math.random() * number.length)];
+
+let getRndSymbol = symbol[Math.floor(Math.random() * symbol.length)];
+
+// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
